5c7a5a19a9f0cd31ad81a3ea05d0c192a6e5e578
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-08-24 12:46:34 +0530

    Time: 17 ms (17.4%), Space: 46.5 MB (5.39%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mp[i-j].push_back(grid[i][j]);
+            }
+        }
+        for(auto &it:mp){
+            int key=it.first;
+            vector<int>&v=it.second;
+            if(key>=0) sort(v.rbegin(),v.rend());
+            else sort(v.begin(),v.end());
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j]=mp[i-j].front();
+                mp[i-j].erase(mp[i-j].begin());
+            }
+        }
+        return grid;
+    }
+};
