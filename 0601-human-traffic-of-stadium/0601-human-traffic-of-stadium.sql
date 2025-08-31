77d5e26e98dde607b8446a62ecdfc7b76f29abbf
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-08-31 15:36:38 +0530

    Time: 450 ms (14.77%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,8 @@
+with grp_number as(
+select *, row_number() over(order by visit_date) as rn,id-row_number() over (order by visit_date) as grp
+from stadium
+where people>=100)
+select id,visit_date,people from grp_number
+where grp in (
+    select grp from grp_number group by grp having count(1)>=3
+)
