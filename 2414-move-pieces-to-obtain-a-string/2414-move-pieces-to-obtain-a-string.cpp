ec72a422f09c7cbffe8b08e18c88457d06db2441
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-08 18:41:31 +0530

    Time: 5 ms (89.01%), Space: 21.7 MB (92.07%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int l=0,r=0;
+        for(int i=0;i<start.size();i++){
+            if(start[i]=='L') l--;
+            else if(start[i]=='R'){
+                r++;
+                if(l!=0) return false;
+            }
+            if(target[i]=='L') {
+                l++;
+                if(r!=0) return false;
+            }
+            else if(target[i]=='R') r--;
+            if(l<0 || r<0) return false;
+        }
+        return l==0 && r==0;
+    }
+};
