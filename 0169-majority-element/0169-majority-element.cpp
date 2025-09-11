3321bf82d1751c06a33d63c083c0cd8ed6d847c7
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-11 23:43:54 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.24%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el,count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                count=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el) count++;
+            else count--;
+        }
+        return el;
+    }
+};
