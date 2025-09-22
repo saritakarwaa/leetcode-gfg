e4aa3dbbdc795f25f8cd05ed59776951c2cafc40
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-22 21:20:38 +0530

    Time: 8 ms (38.16%), Space: 14 MB (53.46%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows==1 || numRows>=s.size()) return s;
-        string res="";
-        int cycle=2*numRows-2;
-        for(int i=0;i<s.size();i+=cycle){
-            res+=s[i];
+        if(numRows==1) return s;
+        vector<string>res(numRows);
+        int i=0;
+        while(i<s.size()){
+            for(int idx=0;idx<numRows && i<s.size();idx++) res[idx]+=s[i++];
+            for(int idx=numRows-2;idx>0 && i<s.size();idx--) res[idx]+=s[i++];
         }
-        for (int row = 1; row < numRows - 1; row++) {      // For each middle row
-            for (int i = row; i < s.size(); i += cycle) {  // Start at row position, jump by cycle
-                res += s[i];                               // First character in this cycle
-                int nextIndex = i + cycle - 2 * row;       // Calculate second character position
-                if (nextIndex < s.size()) {
-                    res += s[nextIndex];                   // Second character in this cycle
-                }
-            }
+        string ans="";
+        for(int j=0;j<res.size();j++){
+            ans+=res[j];
         }
-        for(int i=numRows-1;i<s.size();i+=cycle){
-            res+=s[i];
-        }
-        return res;
+        return ans;
     }
 };
