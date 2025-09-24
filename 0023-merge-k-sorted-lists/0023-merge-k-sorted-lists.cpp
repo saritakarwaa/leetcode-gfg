cf10c5cd67999972b08e955421821a1677a91a34
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-24 22:19:51 +0530

    Time: 3 ms (63.87%), Space: 18.6 MB (41.08%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,21 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int>l;
-        for(auto list:lists){
-            while(list){
-                l.push_back(list->val);
-                list=list->next;
-            }
+    ListNode* mergeTwoSortedLists(ListNode* l1,ListNode* l2){
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoSortedLists(l1->next,l2);
+            return l1;
         }
-        sort(l.begin(),l.end());
-        ListNode *newhead=new ListNode(0);
-        ListNode *temp=newhead;
-        for(int val:l){
-            temp->next=new ListNode(val);
-            temp=temp->next;
-        }   
-        return newhead->next;
+        else{
+            l2->next=mergeTwoSortedLists(l1,l2->next);
+            return l2;
+        }
+        return NULL;
+    }
+    ListNode* partitionAndMerge(int start,int end,vector<ListNode*>&lists){
+        if(start>end) return NULL;
+        if(start==end) return lists[start];
+        int mid=start+(end-start)/2;
+        ListNode* l1=partitionAndMerge(start,mid,lists);
+        ListNode* l2=partitionAndMerge(mid+1,end,lists);
+        return mergeTwoSortedLists(l1,l2);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // vector<int>l;
+        // for(auto list:lists){
+        //     while(list){
+        //         l.push_back(list->val);
+        //         list=list->next;
+        //     }
+        // }
+        // sort(l.begin(),l.end());
+        // ListNode *newhead=new ListNode(0);
+        // ListNode *temp=newhead;
+        // for(int val:l){
+        //     temp->next=new ListNode(val);
+        //     temp=temp->next;
+        // }   
+        // return newhead->next;
+        if(lists.size()==0) return NULL;
+        return partitionAndMerge(0,lists.size()-1,lists);
     }
 };
