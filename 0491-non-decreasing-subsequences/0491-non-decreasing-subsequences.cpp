2e70c7dbcab0c6a9d8ee9c1d2100a3edcb781300
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-25 14:42:47 +0530

    Time: 22 ms (88.58%), Space: 26.4 MB (72.75%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void solve(int idx, vector<int>& nums, vector<int>& res, vector<vector<int>>& ans) {
-        if (res.size() >= 2) {
+    void solve(int idx,vector<int>&nums,vector<int>&res,vector<vector<int>>&ans){
+        if(res.size()>=2){
             ans.push_back(res);
         }
-
-        unordered_set<int> used; // track duplicates at this recursion level
-        for (int i = idx; i < nums.size(); i++) {
-            if ((res.empty() || res.back() <= nums[i]) && !used.count(nums[i])) {
-                res.push_back(nums[i]);
-                solve(i + 1, nums, res, ans);
+        unordered_set<int>used;
+        for(int i=idx;i<nums.size();i++){
+            if((res.empty() || res.back()<=nums[i]) && !used.count(nums[i])){
+                 res.push_back(nums[i]);
+                solve(i+1,nums,res,ans);
                 res.pop_back();
                 used.insert(nums[i]);
             }
         }
     }
-
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<int> res;
-        vector<vector<int>> ans;
-        solve(0, nums, res, ans);
+        vector<int>res;
+        vector<vector<int>>ans;
+        solve(0,nums,res,ans);
         return ans;
     }
 };
