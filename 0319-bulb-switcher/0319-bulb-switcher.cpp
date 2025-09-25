a51906ece227f26585b45da20548f2328823d850
Sarita Karwa <148526176+saritakarwaa@users.noreply.github.com>
2025-09-25 11:31:09 +0530

    Time: 0 ms (100%), Space: 7.9 MB (35.37%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        int count=0;
-        for(int i=1;i*i<=n;i++) count++;
-        return count;
+        // int count=0;
+        // for(int i=1;i*i<=n;i++) count++;
+        // return count;
+        return sqrt(n);
     }
 };
